493a3956bb9c33855b89bed3c6bf6b2291d3ac1f
DIA <informatech69@gmail.com>
2017-10-15 23:59:45 +0200

    AJout de appendEventListener sur input et le bouton search+ réorganisation du head

--- index.php
+++ index.php
@@ -1,43 +1,65 @@
 <!Doctype html>
-<html lang="fr">
+<html lang="fr" xmlns:og="http://ogp.me/ns#">
 <head>
-	<meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <!--un rendu facile à lire, sans zoom,viewport même largeur que l'écran -->
+	    <meta charset="utf-8">
+	    <!-- pk la balise <meta http-equiv=X-UA-Compatible....>? À la base, le mode de compatibilité est prévu pour pouvoir faire fonctionner les vieux sites qui ont été conçus à la mode du tag soup et des tableaux de présentation. Ces derniers se fichaient bien du respect des standards (triste époque du « optimisé pour IE  »).
+           Idéalement, la possibilité d'utiliser ce mode devrait être laissée, toutefois, il est parfois pénible de chercher pendant une heure ce qui peut ne pas fonctionner chez un utilisateur alors que le site a été conçu dans les règles de l'art (comprenez en respectant les standards, testé sur des versions d'Internet Explorer raisonnablement récentes).
+         Toutefois, il est possible d'interdire cette possibilité, et de plusieurs manières :
+         via un méta-tag : <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+         via un entête PHP : header('x-ua-compatible: ie=edge');
+         ou soit directement dans un fichier .htaccess via : header set X-UA-Compatible "IE=Edge".
+         Il faut l'entourer de commentaire if..pour qu'il soit validé par w3 -->
+        <!--[if IE]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <![endif]-->
+        <!--<meta name="viewport ...>? La largeur que vous définissez dans votre « viewport » revient à dire que c'est
+        la largeur qui permet l'affichage le plus optimisé de votre site.
+        La meilleure solution pour que votre site ait un « responsive design » est de définir comme largeur de « viewport » la largeur du matériel utilisé pour visualiser la page Web.
+        Pour vous assurer que votre page s'affichera sans zoom, vous pouvez utiliser la propriété inital-scale pour définir le zoom par défaut.
+        En mettant l'échelle à 1, on s'assure qu'aucun zoom ne sera fait à l'affichage de la page.
+        Si vous souhaitez empêcher tout zoom de la part de l'utilisateur, vous pouvez définir la propriété maximum-scale à 1 afin d'empêcher tout redimensionnement. *d'info: voir http://paulund.developpez.com/tutoriels/html/metatag-viewport/-->
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- Global Site Tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-106866773-1"></script>
-        <script>
-         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments)};
-       gtag('js', new Date());
-       gtag('config', 'UA-106866773-1');
-       </script>
-
-	<title>index</title>
+        <!-- Global Site Tag (gtag.js) - Google Analytics: Google me demande de mettre ces lignes de code entre les balises HEAD de mon site pour activer l'analitic. On va mettre le code dans un fichier php séparé -->
+        <?php require_once 'google_analytic.php'; ?>
         <!-- Une feuille de styles de base pour bien démarrer vos projets:knass avec alsecration-->
         <link rel="stylesheet" type="text/css" media="all" href="css/knass.css" />
         <!--Lien vers fichier Bootstrap.min.css-->
         <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
         <!--Lien vers feuille de style.css-->
         <link rel="stylesheet" type="text/css" href="style.css">
-        <!-- media queries :Pour ceux qui ont une résolution inférieure à 980px-->
+        <!-- media queries: Pour ceux qui ont une résolution inférieure à 980px-->
         <link rel="stylesheet" media="screen and (max-width: 980px)" href="mediaqueries.css" />
         <!--Lien vers jquery3.2.1-->
         <script type="text/javascript" src="jquery-3.2.1.min.js"></script>
         <link rel="stylesheet" href="YouTubePopUp.css">
 
-	<!--Lien vers la police-->
-        <lixcnk hcxref="https://fonts.googleapis.com/css?family=Patrick+Hand" rel="stylesheet">
+	    <!--Lien vers la police-->
+        <link href="https://fonts.googleapis.com/css?family=Patrick+Hand" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
         <!--Lien pour Icones google-->
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"> 
-        <!--Favicon -Début-->
+        <!--Favicon -->
         <?php include'favicon.php';?>
-
+        <!--La balise <title> correspond au titre de votre page web. Il s’agit d’un champ essentiel à ne pas négliger dans le cadre d’une bonne stratégie d’optimisation du référencement naturel puisqu’elle est l’un des critères les plus importants pour les moteurs de recherche (Google, Bing...)
+            Le contenu de la balise <title> d’une page web apparaît dans la partie supérieure de la fenêtre de navigation ainsi que dans le nom des onglets. Note: placez les balises title et meta description juste avant </head> et la balise title au dessus de meta desc. Pour +d'info voir: https://www.yakaferci.com/balise-title/-->
+	    <title>Accueil:tutoriels sur les langages web</title>
+	    <title><?php echo $_GET["\'id\'"];?></title>
+	    <!--En théorie, l'impact de l'Open Graph sur le SEO est limité. En effet, le protocole Open Graph est destiné à l'origine aux réseaux sociaux (Facebook, Google +, Twitter...) et non aux moteurs de recherche (Google, Bing...)
+           En pratique cependant, les réseaux sociaux comme Facebook ont pris une telle importance depuis quelques années que les moteurs de recherche ne peuvent pas ignorer les données de l'Open Graph dans leurs algorithmes.Pour mettre en place le protocole Open Graph, il faut simplement :
+            Ajouter le namespace "http://ogp.me/ns#" au tag html
+            Au sein des balises head, il faut rajouter des balises meta avec les attributs property et content
+            Les spécifications définissent 4 balises de l'Open Graph comme "Requise" : og:title, og:type, og:url, og:image
+            D'autres tags sont aussi utiles, notamment si votre page a du contenu riche (vidéo). Pour +info voir:https://www.yakaferci.com/open-graph/-->
+        <meta property="og:title" content="Article sur l'Open Graph" />
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content="http://www.langageweb.com/open-graph/" />
+        <meta property="og:image" content="http://www.langageweb.com/images/logo.jpg" />
+        <!--La balise meta et plus précisément la balise meta description permet d’ajouter une description d'une page affichée, ainsi que des mots-clés pour lesquels on souhaite optimiser une page.
+        Elle permet d’informer les moteurs de recherche ainsi que les internautes du contenu de la page. Afin de tirer profit de cette balise (dans le cadre du référencement naturel) il y a quelques règles à respecter et quelques pièges à éviter. voir:https://www.yakaferci.com/balise-meta-description/-->
+        <meta name="description" content="Apprendre facilement les langages de créations de sites web -HTML/CSS/JAVASCRIPT/PHP/JQUERY..avec les meilleurs vidéastes français développeurs web sur youtube.">
 
 </head>
-<body onload="alert('Votre taille d\'écran est de '+screen.width)">
+<body>
     <header>
         <?php include'headerIndex.php';?>
     </header>
@@ -101,10 +123,9 @@
 <footer>
   <?php include 'footer.php';?>
 </footer>
+<script src="app.js"></script>
 <script>
 jQuery("a.demo").YouTubePopUp({ autoplay: 0 });
 </script>
 </body>
 </html>
-
-
