0d1aecf67df568b059a33a8bf33b97a255fdaab2
Dia <informatech69@gmail.com>
2018-02-22 00:57:15 +0100

    Ajout d'une video sur Ajax

---
+++ images-icones..de mon site/favicon/google_analytic.php
@@ -0,0 +1,11 @@
+<?php 
+ echo '<script async src="https://www.googletagmanager.com/gtag/js?id=UA-106866773-1"></script>
+        <script>
+         window.dataLayer = window.dataLayer || [];
+         function gtag(){dataLayer.push(arguments)};
+         gtag(\'js\', new Date());
+         gtag(\'config\', \'UA-106866773-1\');
+       </script>'
+ ?>
+
+ <!-- Global Site Tag (gtag.js) - Google Analytics: Google me demande de mettre ces lignes de code entre les balises HEAD de mon site pour activer l'analitic. On va mettre le code dans un fichier php séparé -->
